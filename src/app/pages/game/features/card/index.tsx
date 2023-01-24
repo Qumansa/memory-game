d627ee36c4cddb0822159ae2313ef6ddf321c8aa
Qumansa <qumansa@yandex.ru>
2023-01-24 23:02:10 +0300

    favicon, base.scss, variables.scss
    - изменена фавиконка;
    - удалены ненужные стили.

--- src/app/pages/game/features/card/index.tsx
+++ src/app/pages/game/features/card/index.tsx
@@ -81,12 +81,12 @@ export const Card = ({ id, src, isActive, isFound }: CardProps) => {
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
-						stroke-width="1.2"
+						strokeWidth="1.2"
 						stroke="#0f6c76"
 						className={`${styles.cards__check} w-6 h-6`}>
 						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 							d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
 						/>
 					</svg>
